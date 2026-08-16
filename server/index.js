0997ae568e5ca9a75235577ae3f93f435893f03c
SIVA CHARAN KOLLAPURAM GANDLA <kgsivacharan2005@gmail.com>
2026-08-16 18:24:13 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -167,7 +167,7 @@ JSON SCHEMA:
                 }
               ],
               temperature: 0.1,
-              max_tokens: 8000,
+              max_tokens: 4500,
               response_format: { type: 'json_object' }
             })
           }
